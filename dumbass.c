ffbcffc1d11876c0ff3752a3ccc0934f33beeea1
Joshua F <97619681+JoshuaFarmer@users.noreply.github.com>
2023-09-29 13:48:50 +0100

    Update dumbass.c

--- dumbass.c
+++ dumbass.c
@@ -1,7 +1,11 @@
-#include <stdio.h>
+#include <stdio.h> // library
+
+// main function
 int main() {
-  bool dumbass = true;
+  bool dumbass = true; // creates a boolean variable called "dumbass", default value is true
   if (dumbass == true) {
+    // if you are a dumbass, print "you are a dumbass"
     printf("you are a dumbass!");
   }
-}
+  return 0; // returns and ends the program
+} // end of main
